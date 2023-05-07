0a32ec1f6ac7409b4b9c49a0e5fea6c56e2b2ee8
isaacyclai <107823900+isaacyclai@users.noreply.github.com>
2023-05-07 12:31:52 +0800

    Update and rename house.m to MA2213/house.m

--- MA2213/house.m
+++ MA2213/house.m
@@ -1,6 +1,5 @@
 % Householder reflecor
-% x is a vector such that H*x = [norm(x); 0; 0...]. s is (i-1)-th column of 
-% the matrix we are doing QR factorisation on.function HH = house(x,s) 
+% x is a vector such that H*x = [norm(x); 0; 0...]. s is (i-1)-th column of the matrix we are doing QR factorisation on.function HH = house(x,s) 
 function HH = house(x,s)
 L = length(x);
 norm = sqrt(x'*x);
