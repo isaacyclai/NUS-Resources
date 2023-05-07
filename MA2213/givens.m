a995b7d0b744c86412e663052174a21b7fdef27a
isaacyclai <107823900+isaacyclai@users.noreply.github.com>
2023-05-07 12:31:04 +0800

    Update and rename givens.m to MA2213/givens.m

--- MA2213/givens.m
+++ MA2213/givens.m
@@ -1,6 +1,5 @@
 % Givens rotation
-% x is the vector, a is the coordinate used to eliminate, b is the
-% coordinate to be eliminated
+% x is the vector, a is the coordinate used to eliminate, b is the coordinate to be eliminated
 function G = givens(x,b,a)
 c = x(a)/sqrt(x(a)^2 + x(b)^2); % cos
 s = x(b)/sqrt(x(a)^2 + x(b)^2); % sin
